debd331ad40542a984eb8eb56ae7e9f3f2a4dc67
cunhavcs <cunha.vcs@gmail.com>
2022-12-17 09:21:11 -0300

    Resolve ex-01-variaveis

---
+++ 01-fundamentos/04-introducao-a-javascript/ex-01-variaveis/script.js
@@ -0,0 +1,11 @@
+const myName = 'Vinicius';
+const birthCity = 'São Paulo';
+let birthYear = 1989;
+
+console.log('Nome: ' + myName);
+console.log('Cidade de nascimento: ' + birthCity);
+console.log('Ano de nascimento: ' + birthYear);
+
+birthYear = 2000;
+
+console.log('Ano de nascimento alterado: ' + birthYear);
