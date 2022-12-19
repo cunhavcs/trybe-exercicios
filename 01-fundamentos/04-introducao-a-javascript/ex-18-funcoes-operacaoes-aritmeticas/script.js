3df98730911c085753ce47a2eb62dc40ce94e9b4
cunhavcs <cunha.vcs@gmail.com>
2022-12-19 14:27:55 -0300

    Resolve ex-18-funcoes-operacoes-aritmeticas

---
+++ 01-fundamentos/04-introducao-a-javascript/ex-18-funcoes-operacaoes-aritmeticas/script.js
@@ -0,0 +1,11 @@
+const sum = (num1, num2) => num1 + num2;
+const subtraction = (num1, num2) => num1 - num2;
+const multiplication = (num1, num2) => num1 * num2;
+const division = (num1, num2) => num1 / num2;
+const remainder = (num1, num2) => num1 % num2;
+
+console.log(sum(10, 5));
+console.log(subtraction(10, 5));
+console.log(multiplication(10, 5));
+console.log(division(10, 5));
+console.log(remainder(10, 5));
