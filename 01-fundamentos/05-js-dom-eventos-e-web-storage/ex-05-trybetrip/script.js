0cf4a6da541fd06165557354dd68631df99177e4
cunhavcs <cunha.vcs@gmail.com>
2022-12-20 17:27:56 -0300

    Resolve ex-05-trybetrip-parte15

--- 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
+++ 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
@@ -70,4 +70,8 @@ rightSection.style.marginRight = 'auto';
 // 14 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
 centerSection.parentElement.style.backgroundColor = 'green';
 
-// 15 - Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
+// 15 - Remova os dois últimos elementos (nove e dez) da lista criada no passo 8
+const liElements = document.getElementsByTagName('li');
+for (let i = (liElements.length - 1); i > 7 ; i -= 1) {
+  liElements[i].remove();
+}
