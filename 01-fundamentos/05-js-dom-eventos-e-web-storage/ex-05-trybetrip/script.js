eb234a50ec5fb0c7e4a99a78c7cd36aeafc11cd6
cunhavcs <cunha.vcs@gmail.com>
2022-12-20 17:30:24 -0300

    Resolve ex-05-trybetrip

--- 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
+++ 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
@@ -1,41 +1,41 @@
-// 01 - Adicione a tag h1 com o texto TrybeTrip - Agência de Viagens como filho da tag body;
+// Parte 01
 const body = document.getElementsByTagName('body')[0];
 const h1 = document.createElement('h1');
 h1.innerText = 'TrybeTrip - Agência de Viagens';
 body.appendChild(h1);
 
-// 02 - Adicione a tag main com a classe main-content como filho da tag body;
+// Parte 02
 const main = document.createElement('main');
 main.classList.add('main-content');
 body.appendChild(main);
 
-// 03 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+// Parte 03
 const centerSection = document.createElement('section');
 centerSection.classList.add('center-content');
 main.appendChild(centerSection);
 
-// 04 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+// Parte 04
 const p = document.createElement('p');
 p.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit accusantium labore iure facere blanditiis maiores voluptatum eum harum beatae expedita, sed tempore temporibus dolorem neque delectus. Blanditiis commodi rerum facilis.';
 centerSection.appendChild(p);
 
-// 05 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+// Parte 05
 const leftSection = document.createElement('section');
 leftSection.classList.add('left-content');
 main.appendChild(leftSection);
 
-// 06 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+// Parte 06
 const rightSection = document.createElement('section');
 rightSection.classList.add('right-content');
 main.appendChild(rightSection);
 
-// 07 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
+// Parte 07
 const img = document.createElement('img');
 img.src = 'https://picsum.photos/200';
 img.classList.add('small-image');
 leftSection.appendChild(img);
 
-// 08 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+// Parte 08
 const ul = document.createElement('ul');
 const numbers = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
 for (let i = 0; i < numbers.length; i += 1) {
@@ -45,32 +45,31 @@ for (let i = 0; i < numbers.length; i += 1) {
 }
 rightSection.appendChild(ul);
 
-// 09 - Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
+// Parte 09
 for (let i = 0; i < 3; i += 1) {
   const h3 = document.createElement('h3');
   main.appendChild(h3);
 }
 
-// 10 - Adicione a classe title na tag h1 criada;
+// Parte 10
 h1.classList.add('title');
 
-// 11 - Adicione a classe description nas 3 tags h3 criadas;
+// Parte 11
 const h3TypeTitles = document.getElementsByTagName('h3');
 for (let i = 0; i < h3TypeTitles.length; i += 1) {
   h3TypeTitles[i].classList.add('description');
 }
 
-// 12 - Remova a section criada no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild();
+// Parte 12
 main.removeChild(leftSection);
 
-// 13 - Centralize a section criada no passo 6 (aquele que possui a classe right-content).
-// De olho na dica 👀:: Para centralizar a section, basta configurar o margin-right: auto da section;
+// Parte 13
 rightSection.style.marginRight = 'auto';
 
-// 14 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
+// Parte 14
 centerSection.parentElement.style.backgroundColor = 'green';
 
-// 15 - Remova os dois últimos elementos (nove e dez) da lista criada no passo 8
+// Parte 15
 const liElements = document.getElementsByTagName('li');
 for (let i = (liElements.length - 1); i > 7 ; i -= 1) {
   liElements[i].remove();
