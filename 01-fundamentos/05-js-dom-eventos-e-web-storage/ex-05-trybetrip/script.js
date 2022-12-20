51b06bf573a79c67b27e3ca8eae7014ff0830305
cunhavcs <cunha.vcs@gmail.com>
2022-12-20 17:04:06 -0300

    Resolve ex-05-trybetrip-parte5

--- 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
+++ 01-fundamentos/05-js-dom-eventos-e-web-storage/ex-05-trybetrip/script.js
@@ -20,6 +20,10 @@ p.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit a
 centerSection.appendChild(p);
 
 // 05 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+const leftSection = document.createElement('section');
+leftSection.classList.add('left-content');
+main.appendChild(leftSection);
+
 // 06 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 // 07 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
