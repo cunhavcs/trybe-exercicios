cd13e9574385674ff637672921c95ee315d6a153
cunhavcs <cunha.vcs@gmail.com>
2023-01-09 15:06:33 -0300

    Refatora ex-02-image-component

--- 02-front-end/02-introducao-ao-react/ex-02-image-component/src/App.js
+++ 02-front-end/02-introducao-ao-react/ex-02-image-component/src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import { Component } from 'react';
 import Image from './Image';
 import catImg from './cat-img.jpg';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    const altText = 'Cute cat';
-    return (
-      <Image source={ catImg } alternativeText={ altText } />
-    );
+    return <Image source={ catImg } alternativeText="Cute cat staring" />
   }
 }
 
